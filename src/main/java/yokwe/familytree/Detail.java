2af069ec812b8d30594de76613db4609ce086458
hasegawa <hasegawa@192.168.86.34>
2023-03-18 10:53:04 +0900

    Move toDetailList to Detail

--- src/main/java/yokwe/familytree/Detail.java
+++ src/main/java/yokwe/familytree/Detail.java
@@ -1,6 +1,13 @@
 package yokwe.familytree;
 
-public class Detail {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Detail implements Comparable<Detail> {
+	private static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
+
 	enum Type {
 		DEATH,
 		BIRTH,
@@ -11,79 +18,107 @@ public class Detail {
 		HEIR,
 	}
 	
-	public final String       string;
-	//
 	public final JapaneseDate date;
 	public final Type         type;
+	public final String       value;
 	
-	public Detail(String string, JapaneseDate date, Type type) {
-		this.string = string;
+	public Detail(JapaneseDate date, Type type, String value) {
 		this.date   = date;
 		this.type   = type;
+		this.value  = value;
+	}
+	public Detail(JapaneseDate date, Type type) {
+		this(date, type, null);
 	}
 	
-	public static class Death extends Detail {
-		public final String place;
-		
-		public Death(String string, JapaneseDate date, String place) {
-			super(string, date, Type.BIRTH);
-			this.place = place;
+	@Override
+	public String toString() {
+		if (value != null) {
+			return String.format("{%s %s %s}", date, type, value);
+		} else {
+			return String.format("{%s %s}", date, type);
 		}
-		public Death(String string, JapaneseDate date) {
-			this(string, date, null);
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof Detail) {
+			return compareTo((Detail)o) == 0;
+		} else {
+			return false;
 		}
+	}
+	@Override
+	public int compareTo(Detail that) {
+		int ret = this.date.compareTo(that.date);
+		if (ret == 0) ret = this.type.compareTo(that.type);
+		return ret;
+	}
+	
+	
+	public static class M19 {
+		private static final Pattern PAT_DEATH      = Pattern.compile("時(.+?)ニ於テ死亡");
+		private static final Pattern PAT_BIRTH      = Pattern.compile("^(.+?)ニ於テ出生");
+		private static final Pattern PAT_MARRIAGE_A = Pattern.compile("番[戸地](.+?)ト婚姻届出");
+		private static final Pattern PAT_MARRIAGE_B = Pattern.compile("^(.+?)ト婚姻届出");
 		
-		@Override
-		public String toString() {
-			if (place == null) {
-				return String.format("{%s %s}", date, type);
-			} else {
-				return String.format("{%s %s %s}", date, type, place);
+		public static Detail getInstance(String string) {
+			JapaneseDate date = JapaneseDate.getInstance(string);
+			if (date == null) return null;
+			
+			if (string.contains("死亡")) {
+				Matcher m = PAT_DEATH.matcher(string);
+				if (m.find()) {
+					String place = m.group(1);
+//					logger.info("DEATH  {}", place);
+					return new Detail(date, Type.DEATH, place);
+				}
+//				logger.info("## {}", string);
+				return new Detail(date, Type.DEATH);
 			}
-		}
-	}
-	
-	public static class Birth extends Detail {
-		public final String place;
-		
-		public Birth(String string, JapaneseDate date, String place) {
-			super(string, date, Type.BIRTH);
-			this.place = place;
-		}
-		public Birth(String string, JapaneseDate date) {
-			this(string, date, null);
-		}
-		
-		@Override
-		public String toString() {
-			if (place == null) {
-				return String.format("{%s %s}", date, type);
-			} else {
-				return String.format("{%s %s %s}", date, type, place);
+			if (string.contains("出生")) {
+				Matcher m = PAT_BIRTH.matcher(string);
+				if (m.find()) {
+					String place = m.group(1);
+//					logger.info("BIRTH  {}", place);
+					return new Detail(date, Type.BIRTH, place);
+				}
+				return new Detail(date, Type.BIRTH);
 			}
-		}
-	}
-	
-	public static class Marriage extends Detail {
-		public final String spouse;
-		
-		public Marriage(String string, JapaneseDate date, String spouse) {
-			super(string, date, Type.MARRIAGE);
-			this.spouse = spouse;
-		}
-		public Marriage(String string, JapaneseDate date) {
-			this(string, date, null);
-		}
-		
-		@Override
-		public String toString() {
-			if (spouse == null) {
-				return String.format("{%s %s}", date, type);
-			} else {
-				return String.format("{%s %s %s}", date, type, spouse);
+			if (string.contains("入籍")) {
+				if (string.contains("携帯入籍")) return null;
+				return new Detail(date, Type.MARRIAGE);
 			}
+			if (string.contains("婚姻")) {
+				{
+					Matcher m = PAT_MARRIAGE_A.matcher(string);
+					if (m.find()) {
+						String spouse = m.group(1);
+//						logger.info("MARRIAGE  {}", spouse);
+						return new Detail(date, Type.MARRIAGE, spouse);
+					}
+				}
+				{
+					Matcher m = PAT_MARRIAGE_B.matcher(string);
+					if (m.find()) {
+						String spouse = m.group(1);
+//						logger.info("MARRIAGE  {}", spouse);
+						return new Detail(date, Type.MARRIAGE, spouse);
+					}
+				}
+//				logger.info("## {}", string);
+			}
+			logger.info("## {}", string);
+			return null;
+		}
+
+		public static List<Detail> toDetailList(List<String> list) {
+			List<Detail> result = new ArrayList<>();
+			for(var e: list) {
+				Detail detail = getInstance(e);
+				if (detail != null) result.add(detail);
+			}
+			return result;
 		}
 	}
-	
-	
 }
--- src/main/java/yokwe/familytree/FamilyRecord.java
+++ src/main/java/yokwe/familytree/FamilyRecord.java
@@ -2,12 +2,8 @@ package yokwe.familytree;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public abstract class FamilyRecord {
-	private static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
-
 	public final String format;
 	public final String id;       // registerID
 	public final String domicile;
@@ -19,86 +15,21 @@ public abstract class FamilyRecord {
 	}
 	
 	public static class M19 extends FamilyRecord {
-		private static final Pattern PAT_DEATH = Pattern.compile("時(.+?)ニ於テ死亡");
-		private static final Pattern PAT_BIRTH = Pattern.compile("^(.+?)ニ於テ出生");
-		private static final Pattern PAT_MARRIAGE_A = Pattern.compile("番[戸地](.+?)ト婚姻届出");
-		private static final Pattern PAT_MARRIAGE_B = Pattern.compile("^(.+?)ト婚姻届出");
-		
-		public static Detail toDetail(String string) {
-			JapaneseDate date = JapaneseDate.getInstance(string);
-			if (date == null) return null;
-			// FIXME
-			
-			if (string.contains("死亡")) {
-				Matcher m = PAT_DEATH.matcher(string);
-				if (m.find()) {
-					String place = m.group(1);
-//					logger.info("DEATH  {}", place);
-					return new Detail.Death(string, date, place);
-				}
-//				logger.info("## {}", string);
-				return new Detail.Death(string, date);
-			}
-			if (string.contains("出生")) {
-				Matcher m = PAT_BIRTH.matcher(string);
-				if (m.find()) {
-					String place = m.group(1);
-//					logger.info("BIRTH  {}", place);
-					return new Detail.Birth(string, date, place);
-				}
-				return new Detail. Birth(string, date);
-			}
-			if (string.contains("入籍")) {
-				if (string.contains("携帯入籍")) return null;
-				return new Detail. Marriage(string, date);
-			}
-			if (string.contains("婚姻")) {
-				{
-					Matcher m = PAT_MARRIAGE_A.matcher(string);
-					if (m.find()) {
-						String spouse = m.group(1);
-//						logger.info("MARRIAGE  {}", spouse);
-						return new Detail.Marriage(string, date, spouse);
-					}
-				}
-				{
-					Matcher m = PAT_MARRIAGE_B.matcher(string);
-					if (m.find()) {
-						String spouse = m.group(1);
-//						logger.info("MARRIAGE  {}", spouse);
-						return new Detail.Marriage(string, date, spouse);
-					}
-				}
-//				logger.info("## {}", string);
-			}
-			logger.info("## {}", string);
-			return null;
-		}
-		public static List<Detail> toDetailList(List<String> list) {
-			List<Detail> result = new ArrayList<>();
-			for(var e: list) {
-				Detail detail = toDetail(e);
-				if (detail == null) continue;
-				result.add(detail);
-			}
-			return result;
-		}
-		
 		public static class Member {
 			public final String name;
-			public final String relationship;
+			public final String relation;
 			// 戸主　母　妻　長男　二男　三男　長女　二女　三女　婦　孫
-			public final String relationshipToFamily;
+			public final String relationToFamly;
 
 			public final String birthdate;
 			
 			public final List<Detail> details = new ArrayList<>();
 			
-			public Member(String name, String relationship, String relationshipToFamily, String birthdate) {
-				this.name = name;
-				this.relationship = relationship;
-				this.relationshipToFamily = relationshipToFamily;
-				this.birthdate = birthdate;
+			public Member(String name, String relation, String relationToFamily, String birthdate) {
+				this.name            = name;
+				this.relation        = relation;
+				this.relationToFamly = relationToFamily;
+				this.birthdate       = birthdate;
 			}
 			void addDetail(Detail detail) {
 				details.add(detail);
@@ -107,15 +38,15 @@ public abstract class FamilyRecord {
 
 		public static final String FORMAT = "明治19年式";
 		
-		public final String        previousHeadOfFamily;
-		public final Member        headOfFamily;
+		public final String        previousHead;
+		public final Member        head;
 		public final List<Member>  members = new ArrayList<>();
 		
 		public M19(String id, String domicile,
-			String previousHeadOfFamily, Member headOfFamily, List<Member> members) {
+			String previousHead, Member head, List<Member> members) {
 			super(FORMAT, id, domicile);
-			this.previousHeadOfFamily = previousHeadOfFamily;
-			this.headOfFamily         = headOfFamily;
+			this.previousHead = previousHead;
+			this.head         = head;
 		}
 		void addMember(Member member) {
 			members.add(member);
