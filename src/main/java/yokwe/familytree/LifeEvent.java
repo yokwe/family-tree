ae159c341fd8499227edc28bb440df046e667361
hasegawa <hasegawa@192.168.86.34>
2023-03-20 00:07:16 +0900

    Add all handler classes.

--- src/main/java/yokwe/familytree/LifeEvent.java
+++ src/main/java/yokwe/familytree/LifeEvent.java
@@ -2,30 +2,53 @@ package yokwe.familytree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LifeEvent implements Comparable<LifeEvent> {
+	private static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
+
 	enum Type {
-		DEATH,
-		BIRTH,
-		MARRIAGE,
-		BRANCH,
-		RETIREMENT,
-		DISALLOW_INHERIT,
-		ALLOW_INHERIT,
-		INHERIT,
+		BIRTH           ("出生"),
+		DEATH           ("死亡"),
+		MARRIAGE        ("結婚"),
+		ADOPTION        ("養子"),
+		DIVORCE         ("離婚"),
+		BRANCH          ("分家"),
+		RETIREMENT      ("隠居"),
+		DISALLOW_INHERIT("廃嫡"),
+		ALLOW_INHERIT   ("嗣子"),
+		INHERIT         ("相続");
+		
+		final String name;
+		
+		private Type(String name) {
+			this.name = name;
+		}
+		
+		@Override
+		public String toString() {
+			return name;
+		}
 	}
 	
-	public static LifeEvent death(String string, JapaneseDate date, String value) {
-		return new LifeEvent(string, Type.DEATH, date, value);
+	public static LifeEvent birth(String string, JapaneseDate date) {
+		return new LifeEvent(string, Type.BIRTH, date);
 	}
-	public static LifeEvent birth(String string, JapaneseDate date, String value) {
-		return new LifeEvent(string, Type.BIRTH, date, value);
+	public static LifeEvent death(String string, JapaneseDate date) {
+		return new LifeEvent(string, Type.DEATH, date);
 	}
-	public static LifeEvent marriage(String string, JapaneseDate date, String value) {
-		return new LifeEvent(string, Type.MARRIAGE, date, value);
+	public static LifeEvent marriage(String string, JapaneseDate date) {
+		return new LifeEvent(string, Type.MARRIAGE, date);
 	}
-	public static LifeEvent branch(String string, JapaneseDate date, String value) {
-		return new LifeEvent(string, Type.BRANCH, date, value);
+	public static LifeEvent adoption(String string, JapaneseDate date) {
+		return new LifeEvent(string, Type.ADOPTION, date);
+	}
+	public static LifeEvent divorce(String string, JapaneseDate date) {
+		return new LifeEvent(string, Type.DIVORCE, date);
+	}
+	public static LifeEvent branch(String string, JapaneseDate date) {
+		return new LifeEvent(string, Type.BRANCH, date);
 	}
 	public static LifeEvent retirement(String string, JapaneseDate date) {
 		return new LifeEvent(string, Type.RETIREMENT, date);
@@ -105,10 +128,10 @@ public class LifeEvent implements Comparable<LifeEvent> {
 		LifeEvent toLiveEvent(String string);
 	}
 	private static class Pair {
-		final String  keyword;
-		final Handler handler;
-		Pair(String keyword, Handler handler) {
-			this.keyword = keyword;
+		final Matcher  matcher;
+		final Handler  handler;
+		Pair(String pattern, Handler handler) {
+			this.matcher = Pattern.compile(pattern).matcher("");
 			this.handler = handler;
 		}
 	}
@@ -116,30 +139,99 @@ public class LifeEvent implements Comparable<LifeEvent> {
 		private List<Pair> list = new ArrayList<>();
 		
 		public Converter() {
+			addHandler(Death.KEYWORD,           new Death());
+			addHandler(Birth.KEYWORD,           new Birth());
+			addHandler(Marriage.KEYWORD,        new Marriage());
+			addHandler(Adoption.KEYWORD,        new Adoption());
+			addHandler(Divorce.KEYWORD,         new Divorce());
+			addHandler(Branch.KEYWORD,          new Branch());
+			addHandler(Retirement.KEYWORD,      new Retirement());
 			addHandler(DisallowInherit.KEYWORD, new DisallowInherit());
 			addHandler(AllowInherit.KEYWORD,    new AllowInherit());
 			addHandler(Inherit.KEYWORD,         new Inherit());
-			addHandler(Retirement.KEYWORD,      new Retirement());
 		}
 		private void addHandler(String keyword, Handler handler) {
 			list.add(new Pair(keyword, handler));
 		}
 		
 		public LifeEvent toLifeEvent(String string) {
-			for(var e: list) {
-				if (string.contains(e.keyword)) return e.handler.toLiveEvent(string);
+			for(var pair: list) {
+				if (pair.matcher.reset(string).find()) return pair.handler.toLiveEvent(string);
 			}
+			logger.info("##  {}", string);
 			return null;
 		}
 	}
 }
 
 
-// DEATH,
-// BIRTH,
-// MARRIAGE,
-// BRANCH,
-// RETIREMENT,
+// DEATH           ("出生"),
+class Birth implements LifeEvent.Handler {
+	public static final String KEYWORD = "出生";
+
+	@Override
+	public LifeEvent toLiveEvent(String string) {
+		JapaneseDate date = JapaneseDate.getInstance(string);
+		if (date == null) return null;
+		return LifeEvent.birth(string, date);
+	}
+}
+// BIRTH           ("死亡"),
+class Death implements LifeEvent.Handler {
+	public static final String KEYWORD = "死亡";
+
+	@Override
+	public LifeEvent toLiveEvent(String string) {
+		JapaneseDate date = JapaneseDate.getInstance(string);
+		if (date == null) return null;
+		return LifeEvent.death(string, date);
+	}
+}
+// MARRIAGE        ("結婚"),
+class Marriage implements LifeEvent.Handler {
+	public static final String KEYWORD = "入籍|婚姻";
+
+	@Override
+	public LifeEvent toLiveEvent(String string) {
+		JapaneseDate date = JapaneseDate.getInstance(string);
+		if (date == null) return null;
+		return LifeEvent.marriage(string, date);
+	}
+}
+// ADOPTION        ("養子"),
+class Adoption implements LifeEvent.Handler {
+	public static final String KEYWORD = "養子";
+
+	@Override
+	public LifeEvent toLiveEvent(String string) {
+		JapaneseDate date = JapaneseDate.getInstance(string);
+		if (date == null) return null;
+		return LifeEvent.adoption(string, date);
+	}
+}
+// DIVORCE         ("離婚"),
+class Divorce implements LifeEvent.Handler {
+	public static final String KEYWORD = "離婚";
+
+	@Override
+	public LifeEvent toLiveEvent(String string) {
+		JapaneseDate date = JapaneseDate.getInstance(string);
+		if (date == null) return null;
+		return LifeEvent.divorce(string, date);
+	}
+}
+// BRANCH          ("分家"),
+class Branch implements LifeEvent.Handler {
+	public static final String KEYWORD = "分家";
+
+	@Override
+	public LifeEvent toLiveEvent(String string) {
+		JapaneseDate date = JapaneseDate.getInstance(string);
+		if (date == null) return null;
+		return LifeEvent.branch(string, date);
+	}
+}
+// RETIREMENT      ("隠居"),
 class Retirement implements LifeEvent.Handler {
 	public static final String KEYWORD = "隠居";
 
@@ -150,7 +242,7 @@ class Retirement implements LifeEvent.Handler {
 		return LifeEvent.retirement(string, date);
 	}
 }
-// DISALLOW_INHERIT,
+// DISALLOW_INHERIT("廃嫡"),
 class DisallowInherit implements LifeEvent.Handler {
 	public static final String KEYWORD = "廃嫡";
 
@@ -161,9 +253,9 @@ class DisallowInherit implements LifeEvent.Handler {
 		return LifeEvent.disallowInherit(string, date);
 	}
 }
-// ALLOW_INHERIT,
+// ALLOW_INHERIT   ("嗣子"),
 class AllowInherit implements LifeEvent.Handler {
-	public static final String KEYWORD = "嗣子願済";
+	public static final String KEYWORD = "嗣子";
 
 	@Override
 	public LifeEvent toLiveEvent(String string) {
@@ -172,7 +264,7 @@ class AllowInherit implements LifeEvent.Handler {
 		return LifeEvent.allowInherit(string, date);
 	}
 }
-// INHERIT,
+// INHERIT         ("相続");
 class Inherit implements LifeEvent.Handler {
 	public static final String KEYWORD = "相続";
 
